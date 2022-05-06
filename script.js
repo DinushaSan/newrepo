1d33584e892b86f0549fc7efadef83f021b6c0ab
unknown <dinusha9115eg@gmail.com>
2022-05-06 23:39:59 +0530

    Assessment completed part 2

--- script.js
+++ script.js
@@ -2,3 +2,28 @@
 function shuffleArray(array){
  
 }
+
+var arr = [1,2,3];
+console.log(arr);
+arr.length = 0;
+console.log(arr);
+
+//array to string
+let fruits = ["banana", "apple", "orange", "grape"];
+console.log(fruits.toString());
+
+//connect by an operator
+console.log(fruits.join("*"));
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push("kiwi");
+console.log(fruits);
+
+//shift(removing the first element)
+fruits.shift();
+console.log(fruits);
+//unshift(add element to the beginning)
+fruits.unshift("guava");
+console.log(fruits);
